6b226da3b014f6a1936c07a793dfc1300975b6c6
AlexAutomates <alex@everydayworkflows.com>
2026-05-14 20:54:14 -0400

    chore: normalize version to 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.leantypechirp.keyboard"
         minSdk = 21
         targetSdk = 35
-        versionCode = 3707
-        versionName = "3.7.8"
+        versionCode = 10000
+        versionName = "1.0.0"
 
         proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         
